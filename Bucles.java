e9faf3427994e4a5ea7c0222490f5b3718c9145b
Josias <jsmdndev@gmail.com>
2025-05-27 15:49:58 -0300

    Bucles y Funciones

---
+++ Bucles.java
@@ -0,0 +1,31 @@
+public class Bucles {
+    public static void main(String[] args) {
+        // bucle for
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1);
+        }
+
+        // for each
+        int[] nums = {1, 2 ,3, 4, 5};
+        for (int i: nums) {
+            System.out.println(i * 2);
+        }
+
+        // while
+        int x = 0;
+        while (x == 0) {
+            x++;
+        }
+
+        // do-while
+        do {
+            System.out.println(x);
+            x++;
+        } while (x == 0);
+
+        // Control de ciclo
+        // - continue = salta a la siguiente iteracion
+        // - break = termina el ciclo
+
+    }
+}
---
+++ Funciones.java
@@ -0,0 +1,40 @@
+public class Funciones {
+
+    public static void main(String[] args) {
+        function();
+
+        int num = addFunction(5, 10);
+        // ahora: num = 15
+
+        greet("Juan"); // llama a funcion 1
+        greet("Diego", "Lucas"); // llama a funcion 2
+    }
+
+
+    // funcion
+    public static void function() {
+        System.out.println("this is a function");
+    }
+    // public: se puede acceder desde cualquier parte del codigo
+    // static: la funcion es de tipo static
+    // void: la funcion no retorna nada
+
+
+    // función con retorno + parametros
+    public static int addFunction(int x, int y) {
+        return x + y;
+    }
+
+
+    // sobrecarga de funciones
+    // - sobrecarga se refiere a cuando dos funciones con el mismo nombre ejecutan logica diferente según los paramatros dados
+    public static void greet(String name) { // funcion 1
+        System.out.println("Hello " + name);
+    }
+
+    public static void greet(String name1, String name2) { // funcion 2
+        System.out.println("Hello " + name1 + " and " + name2);
+    }
+    // en este caso se ejecutará una de las funciones dependiendo de los parametros dados
+
+}
