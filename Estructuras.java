a0198a1ae4cd8a5e694dc4aed0ba09a882895a73
Josias <jsmdndev@gmail.com>
2025-05-27 14:47:34 -0300

    Estructuras de Datos Pt.2: set y map

--- Estructuras.java
+++ Estructuras.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Estructuras {
     public static void main(String[] args) {
@@ -49,5 +51,58 @@ public class Estructuras {
 
 
         // set
+        // - lista desordenada
+        // - no se pueden repetir elementos
+        // - no tiene acceso por indices
+        HashSet<Integer> nums = new HashSet<>();
+
+        // add() ... agregar elementos
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        // ahora: nums = (1, 2, 3);
+
+        // size() ... tamaño del set
+        System.out.println(nums.size()); // --> 3
+
+        // contains() ... verifica si un elemento esta incluido
+        System.out.println(nums.contains(3)); // --> true
+
+        // remove() ... elimina el elemento dado
+        nums.remove(3);
+        // ahora: nums = (1, 2);
+
+
+
+        // map
+        // - lista que guarda elementos de tipo clave-valor
+        // - estructura desordenada
+        // - no admite repetidos
+        HashMap<String, Integer> ages = new HashMap<>();
+        // El primer valor String define a todos las claves como cadenas
+        // el segundo valor Integer define a todos los valores como enteros
+
+        // put() ... agregar pares al map
+        ages.put("Diego", 17);
+        ages.put("Juan", 18);
+        ages.put("Sergio", 20);
+
+        // size() ... tamaño del map (cantidad de pares)
+        System.out.println(ages.size()); // --> 3
+
+        // get() ... acceder al valor de una clave dada
+        // ! si la clave no existe devuelve null !
+        int diegoEdad = ages.get("Diego");
+        // ahora: diegoEdad = 17
+
+        // containsKey() ... verifica si la clave dada esta incluida en el map
+        System.out.println(ages.containsKey("Sergio")); // --> true
+
+        // containsValue() ... verifica si un valor pertenece al map
+        System.out.println(ages.containsValue(19)); // --> false
+
+        // remove() ... elimina un par
+        ages.remove("Juan");
+        // ahora: ages = {Diego = 17, Sergio = 20}
     }
 }
